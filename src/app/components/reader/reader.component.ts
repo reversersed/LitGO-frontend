cf8f91574a0d84d9be993fa5ef4c5f6ddb9c422b
reversersed <myregisg@gmail.com>
2025-03-30 23:03:03 +0300

    added reader style editing

--- src/app/components/reader/reader.component.ts
+++ src/app/components/reader/reader.component.ts
@@ -21,6 +21,26 @@ import * as EPUB from 'epubjs';
 import Section from 'epubjs/types/section';
 import { Subscription } from 'rxjs';
 
+interface Style {
+  body: {
+    'overflow-x': string;
+    'overflow-y': string;
+    width: string;
+    'max-width': string;
+    margin: string;
+    padding: string;
+    'box-sizing': string;
+    color?: string;
+    'line-height'?: string;
+  };
+  '*': {
+    'font-family'?: string;
+  };
+  customSettings: {
+    font: string;
+  };
+}
+
 @Component({
   selector: 'app-reader',
   standalone: true,
@@ -32,6 +52,8 @@ export class ReaderComponent implements OnInit, OnDestroy, OnChanges {
   @Input() bookFileUrl!: ArrayBuffer;
   @Input() currentChapter?: EPUB.NavItem;
   @Output() bookContent = new EventEmitter<EPUB.NavItem[]>();
+  @Output() isBookLoading = new EventEmitter<boolean>();
+  @Output() onStyleChanged = new EventEmitter<any>();
 
   router = inject(Router);
   route = inject(ActivatedRoute);
@@ -52,7 +74,18 @@ export class ReaderComponent implements OnInit, OnDestroy, OnChanges {
   paramsSubscription?: Subscription;
   selectedChapter?: EPUB.NavItem;
 
+  currentStyle: any = this.getDefaultStyle();
+
   async ngOnInit(): Promise<void> {
+    let styleProp = localStorage.getItem('reader-style-line-height');
+    if (styleProp !== null) this.currentStyle.body['line-height'] = styleProp;
+
+    styleProp = localStorage.getItem('reader-style-font-family');
+    if (styleProp) this.currentStyle['*']['font-family'] = styleProp;
+
+    styleProp = localStorage.getItem('reader-style-font-size');
+    if (styleProp) this.currentStyle.customSettings['font'] = styleProp;
+
     try {
       this.book = EPUB.default(this.bookFileUrl);
       await this.book.ready;
@@ -64,8 +97,9 @@ export class ReaderComponent implements OnInit, OnDestroy, OnChanges {
         spread: 'none',
         overflow: 'auto',
       });
-      if (this.isDarkTheme()) this.rendition.themes.default('reader.dark.css');
-      else this.rendition.themes.default('reader.css');
+
+      this.updateStyle();
+
       await this.book.loaded.spine;
       await this.book.loaded.navigation;
 
@@ -82,6 +116,7 @@ export class ReaderComponent implements OnInit, OnDestroy, OnChanges {
       this.router.navigate(['/notfound']);
     } finally {
       this.isLoading = false;
+      this.isBookLoading.emit(false);
     }
 
     this.paramsSubscription = this.route.queryParams.subscribe({
@@ -100,7 +135,6 @@ export class ReaderComponent implements OnInit, OnDestroy, OnChanges {
   }
 
   ngOnDestroy(): void {
-    console.log(this.currentSection);
     this.book.destroy();
     this.paramsSubscription?.unsubscribe();
   }
@@ -118,7 +152,46 @@ export class ReaderComponent implements OnInit, OnDestroy, OnChanges {
       await this.rendition?.display(this.selectedChapter.href);
     else await this.rendition?.display(this.sections[index]);
   }
+  saveStyle() {
+    if (this.currentStyle.body['line-height'])
+      localStorage.setItem(
+        'reader-style-line-height',
+        this.currentStyle.body['line-height'].toString()
+      );
 
+    if (this.currentStyle['*']['font-family'])
+      localStorage.setItem(
+        'reader-style-font-family',
+        this.currentStyle['*']['font-family']
+      );
+
+    if (this.currentStyle.customSettings['font'])
+      localStorage.setItem(
+        'reader-style-font-size',
+        this.currentStyle.customSettings['font']
+      );
+  }
+  getDefaultStyle(): Style {
+    return {
+      body: {
+        'overflow-x': 'hidden !important',
+        'overflow-y': 'auto !important',
+        width: '90% !important',
+        'max-width': '100% !important',
+        margin: '0 auto !important',
+        padding: '0 !important',
+        'box-sizing': 'border-box !important',
+        color: this.isDarkTheme() ? 'rgb(180, 180, 190)' : undefined,
+        'line-height': '1.5',
+      },
+      '*': {
+        'font-family': 'LiberEmb !important',
+      },
+      customSettings: {
+        font: '16px',
+      },
+    };
+  }
   nextPage() {
     if (this.currentSection >= this.totalSections - 1) return;
     this.selectedChapter = undefined;
@@ -156,5 +229,21 @@ export class ReaderComponent implements OnInit, OnDestroy, OnChanges {
   }
   isDarkTheme() {
     return window.matchMedia('(prefers-color-scheme: dark)').matches;
+  }
+  updateStyle() {
+    this.onStyleChanged.emit(this.currentStyle);
+    this.rendition.themes.default(this.currentStyle);
+    this.rendition.themes.fontSize(this.currentStyle.customSettings.font);
+    this.saveStyle();
+  }
+  changeStyleParam(
+    section: string,
+    style: string,
+    value: string,
+    restore?: boolean
+  ) {
+    if (restore) this.currentStyle = this.getDefaultStyle();
+    else this.currentStyle[section][style] = value;
+    this.updateStyle();
   }
 }
--- src/app/pages/reader-page/reader-page.component.ts
+++ src/app/pages/reader-page/reader-page.component.ts
@@ -1,4 +1,11 @@
-import { Component, computed, inject, OnDestroy, OnInit } from '@angular/core';
+import {
+  Component,
+  computed,
+  inject,
+  OnDestroy,
+  OnInit,
+  ViewChild,
+} from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { UserService } from '../../service/http/user.service';
 import { ActivatedRoute, Router, RouterLink } from '@angular/router';
@@ -7,7 +14,13 @@ import { BookService } from '../../service/http/book.service';
 import { catchError, map, Observable, of, Subscription } from 'rxjs';
 import Book from '../../models/book.model';
 import { ReaderComponent } from '../../components/reader/reader.component';
-import { faBars, faClose, faSpinner } from '@fortawesome/free-solid-svg-icons';
+import {
+  faAngleUp,
+  faBars,
+  faClose,
+  faRotate,
+  faSpinner,
+} from '@fortawesome/free-solid-svg-icons';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 import { NavItem } from 'epubjs';
 
@@ -18,6 +31,8 @@ import { NavItem } from 'epubjs';
   templateUrl: './reader-page.component.html',
 })
 export class ReaderPageComponent implements OnInit, OnDestroy {
+  @ViewChild(ReaderComponent) reader?: ReaderComponent;
+
   userService = inject(UserService);
   route = inject(ActivatedRoute);
   router = inject(Router);
@@ -37,8 +52,134 @@ export class ReaderPageComponent implements OnInit, OnDestroy {
   faMenu = faBars;
   faClose = faClose;
   faSpinner = faSpinner;
+  faExpand = faAngleUp;
+  faDefault = faRotate;
   selectedChapter: NavItem | undefined;
   bookChapters?: NavItem[];
+  readerStyle?: any;
+
+  currentExpandedSection?: string;
+  settingsExpanded = false;
+  styleSetting = [
+    {
+      name: 'Шрифт',
+      topSection: '*',
+      section: 'font-family',
+      values: [
+        {
+          label: 'Times New Roman',
+          value: 'Times New Roman !important',
+        },
+        {
+          label: 'LiberEmb',
+          value: 'LiberEmb !important',
+        },
+        {
+          label: 'Lucida Console',
+          value: '"Lucida Console" !important',
+        },
+        {
+          label: 'Arial',
+          value: 'Arial !important',
+        },
+        {
+          label: 'Verdana',
+          value: 'Verdana !important',
+        },
+        {
+          label: 'Courier',
+          value: 'Courier !important',
+        },
+        {
+          label: 'FreeMono',
+          value: 'FreeMono !important',
+        },
+      ],
+    },
+    {
+      name: 'Размер текста',
+      topSection: 'customSettings',
+      section: 'font',
+      values: [
+        {
+          label: 'Крошечный',
+          value: '10px',
+        },
+        {
+          label: 'Очень маленький',
+          value: '12px',
+        },
+        {
+          label: 'Маленький',
+          value: '14px',
+        },
+        {
+          label: 'Обычный',
+          value: '16px',
+        },
+        {
+          label: 'Большой',
+          value: '18px',
+        },
+        {
+          label: 'Очень большой',
+          value: '20px',
+        },
+        {
+          label: 'Огромный',
+          value: '22px',
+        },
+      ],
+    },
+    {
+      name: 'Интервал',
+      topSection: 'body',
+      section: 'line-height',
+      values: [
+        {
+          label: '1',
+          value: '1',
+        },
+        {
+          label: '1.15',
+          value: '1.15',
+        },
+        {
+          label: '1.25',
+          value: '1.25',
+        },
+        {
+          label: '1.5',
+          value: '1.5',
+        },
+        {
+          label: '1.75',
+          value: '1.75',
+        },
+        {
+          label: '2',
+          value: '2',
+        },
+        {
+          label: '2.25',
+          value: '2.25',
+        },
+        {
+          label: '2.5',
+          value: '2.5',
+        },
+        {
+          label: '2.75',
+          value: '2.75',
+        },
+        {
+          label: '3',
+          value: '3',
+        },
+      ],
+    },
+  ];
+  isLoading = true;
 
   ngOnInit() {
     this.userService.Auth().subscribe();
